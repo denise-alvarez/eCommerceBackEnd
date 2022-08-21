c41ad628c64e892912208aad408e1528e0a33451
denise-alvarez <denisealvarez3@gmail.com>
2022-08-21 13:56:21 -0700

    update tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+//find all tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  try {
+    const findTags = await Tag.findAll({
+      include: { model: Product},
+    })
+    res.status(200).json(findTags)
+  } catch (err) {
+    res.status(500).json('Something went wrong',err)
+  }
 });
 
 router.get('/:id', (req, res) => {
