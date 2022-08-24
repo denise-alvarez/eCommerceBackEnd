832f46f0de8090f73c20e49e65dcd16c744569f4
denise-alvarez <denisealvarez3@gmail.com>
2022-08-23 17:11:53 -0700

    update product

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,8 @@
 // import important parts of sequelize library
-const { DECIMAL} = require("sequelize");
 const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
 const sequelize = require("../config/connection");
-const { beforeValidate } = require("./Category");
+
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -24,9 +23,6 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
-      validate: {
-        isDecimal: true,
-      }
     },
     stock: {
       type: DataTypes.INTEGER,
@@ -34,9 +30,7 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: id,
+      allowNull: false
       }
     }
   },
